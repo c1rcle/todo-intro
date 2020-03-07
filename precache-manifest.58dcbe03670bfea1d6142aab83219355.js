bdca29bf9752bf94ce2046a1358853fa95baa252
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-03-07 03:07:29 +0100

    Updates

---
+++ precache-manifest.58dcbe03670bfea1d6142aab83219355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212c800a0ea31e61e01e3cbf50055df8",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "e5bb98d9b75e73bece03",
+    "url": "/todo-app/static/css/2.4e228829.chunk.css"
+  },
+  {
+    "revision": "e5bb98d9b75e73bece03",
+    "url": "/todo-app/static/js/2.bd15b2c6.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/todo-app/static/js/2.bd15b2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf739355f244f373aa57",
+    "url": "/todo-app/static/js/main.c4b07f6d.chunk.js"
+  },
+  {
+    "revision": "c339d801fd14056574f5",
+    "url": "/todo-app/static/js/runtime-main.5f90965c.js"
+  }
+]);
